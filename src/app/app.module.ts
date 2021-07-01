5bbedf7b2afe844ca8270f45ec2b6a8f93920167
Samuel Esan <sesn@upei.ca>
2021-07-01 14:48:39 -0300

    feat(app): replaced primeng with ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,24 @@ import { AppComponent } from './app.component';
 import { VisualizerModule } from 'visualizer';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
-import { DropdownModule } from 'primeng/dropdown';
-import { SliderModule } from 'primeng/slider';
-import { RippleModule } from 'primeng/ripple';
-import { SelectButtonModule } from 'primeng/selectbutton';
 import { VisualizerComponent } from './visualizer/visualizer.component';
 import { VisualizerControllerComponent } from './visualizer-controller/visualizer-controller.component';
 import { ControlComponent } from './shared/control/control.component';
-import { CardModule } from 'primeng/card';
-// NG-Zorro
+// NG-Zorro Base
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { HttpClientModule } from '@angular/common/http';
 
-registerLocaleData(en);
+// NG-Zorro Component Modules
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+
+registerLocaleData(en);  // NG-Zorro
 
 @NgModule({
     declarations: [
@@ -35,13 +36,12 @@ registerLocaleData(en);
         BrowserAnimationsModule,
         VisualizerModule,
         FormsModule,
-        ButtonModule,
-        CardModule,
-        DropdownModule,
-        SelectButtonModule,
-        SliderModule,
-        RippleModule,
         HttpClientModule,
+        NzButtonModule,
+        NzDropDownModule,
+        NzIconModule,
+        NzRadioModule,
+        NzSliderModule,
     ],
     providers: [{ provide: NZ_I18N, useValue: en_US }],
     bootstrap: [AppComponent]
