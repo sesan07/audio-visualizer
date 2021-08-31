948e495be283258493c6e3585126455d1e9eb63b
Samuel Esan <sesan@upei.ca>
2021-08-31 18:36:35 -0300

    feat(image-controller): update dimensions on image change

--- src/app/entity-content/image/image-controller/image-controller.component.ts
+++ src/app/entity-content/image/image-controller/image-controller.component.ts
@@ -3,6 +3,7 @@ import { IEntityConfig } from '../../../entity/entity.types';
 import { IImageContentConfig } from '../image.content.types';
 import { ImageSourceService } from '../../../shared/source-services/image.source.service';
 import { ISource } from '../../../shared/source-services/base.source.service.types';
+import { ImageContentService } from '../image.content.service';
 
 @Component({
     selector: 'app-image-controller',
@@ -13,11 +14,12 @@ export class ImageControllerComponent {
 
     @ViewChild('fileInput') fileInputElement: ElementRef<HTMLInputElement>;
 
-    constructor(public imageSourceService: ImageSourceService) {
+    constructor(public imageSourceService: ImageSourceService, private _imageContentService: ImageContentService) {
     }
 
     onSourceChange(source: ISource): void {
         this.entity.entityContentConfig.source = source;
         this.entity.entityContentConfig.element = this.imageSourceService.getSourceElement(source);
+        this._imageContentService.setEntityDimensions(this.entity);
     }
 }
